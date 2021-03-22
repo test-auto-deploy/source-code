6dd42e5b0b4d2bf8c58069bcd6aa3da0a3b735e1
MG <1121506496@qq.com>
2021-03-23 00:17:07 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+    const [tag, setTag] = useState();
+
+    useEffect(() => {
+        const src = document.getElementsByTagName('script')[1].src
+        if(src) {
+            try {
+                const str = src.match(/@(\d|-)*\//)
+
+                if (str) {
+                    setTag(str.substring(1, str.length - 1))
+                }
+            }catch (_){}
+
+        }
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h3>Test auto deploy</h3>
         <p>
-          Init
+            Tag: {tag}
         </p>
 
       </header>
